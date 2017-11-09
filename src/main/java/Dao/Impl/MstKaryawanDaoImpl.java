b026dd50f1d8d8083728d07850f08abe109703ae
Rizky Satria <rizkyprawidia@gmail.com>
2017-11-09 17:06:59 +0700

    Menambah Dao dan Impl

--- src/main/java/Dao/Impl/MstKaryawanDaoImpl.java
+++ src/main/java/Dao/Impl/MstKaryawanDaoImpl.java
@@ -10,36 +10,118 @@ import java.util.List;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
-
-
 import org.springframework.stereotype.Repository;
 
 import entity.MstKaryawan;
 import Dao.MstKaryawanDao;
 
 @Repository("MstKaryawanDao")
-public class MstKaryawanDaoImpl implements MstKaryawanDao{
+public class MstKaryawanDaoImpl implements MstKaryawanDao {
 
 	@Autowired
 	private DataSource dataSource;
-	
+
 	@Override
 	public void save(MstKaryawan mstKaryawan) {
-		
-		
+		String query = "INSERT INTO Table_Karyawan"
+				+ "(NIK,Nama_Karyawan ,Jenis_Kelamin ,No_Absen ,Kode_Perusahaan ,Tanggal_Masuk ,No_Rekening )"
+				+ "VALUES (?,?,?,?,?,?,?)";
+		Connection con = null;
+		PreparedStatement ps = null;
+		try {
+			con = dataSource.getConnection();
+			ps = con.prepareStatement(query);
+
+			ps.setString(1, mstKaryawan.getNik());
+			ps.setString(2, mstKaryawan.getNamaKaryawan());
+			ps.setString(3, mstKaryawan.getJenisKelamin());
+			ps.setString(4, mstKaryawan.getNoAbsen());
+			ps.setString(5, mstKaryawan.getKodePerusahaan());
+			ps.setDate(6, mstKaryawan.getTanggalMasuk());
+			ps.setString(7, mstKaryawan.getNoRekening());
+
+			int out = ps.executeUpdate();
+			if (out != 0) {
+				System.out.println("Sukses");
+			} else {
+				System.out.println("Failed");
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				ps.close();
+				con.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+
 	}
 
 	@Override
 	public void update(MstKaryawan mstKaryawan) {
-		// TODO Auto-generated method stub
-		
+		String query = "UPDATE Table_Karyawan SET Nama_Karyawan = ? ,Jenis_Kelamin = ? ,No_Absen = ? ,Kode_Perusahaan = ? ,Tanggal_Masuk = ? ,No_Rekening = ? "
+				+ " WHERE NIK = ?";
+		Connection con = null;
+		PreparedStatement ps = null;
+		try {
+			con = dataSource.getConnection();
+			ps = con.prepareStatement(query);
+			ps.setString(1, mstKaryawan.getNamaKaryawan());
+			ps.setString(2, mstKaryawan.getJenisKelamin());
+			ps.setString(3, mstKaryawan.getNoAbsen());
+			ps.setString(4, mstKaryawan.getKodePerusahaan());
+			ps.setDate(5, mstKaryawan.getTanggalMasuk());
+			ps.setString(6, mstKaryawan.getNoRekening());
+			ps.setString(7, mstKaryawan.getNik());
+			int out = ps.executeUpdate();
+			if (out != 0) {
+				System.out.println("Sukses");
+			} else {
+				System.out.println("Failed");
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				ps.close();
+				con.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+
 	}
 
 	@Override
 	public void delete(String nik) {
-		// TODO Auto-generated method stub
-		
+		String query = "DELETE FROM Table_Karyawan WHERE NIK = ?";
+		Connection con = null;
+		PreparedStatement ps = null;
+		try {
+			con = dataSource.getConnection();
+			ps = con.prepareStatement(query);
+			ps.setString(1, nik);
+			int out = ps.executeUpdate();
+			if (out != 0) {
+				System.out.println("Karyawan Delete with Id = " + nik);
+
+			} else {
+				System.out.println("No Karyawan Delete with Id = "
+						+ nik);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				ps.close();
+				con.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+
 	}
 
 	@Override
@@ -55,15 +137,18 @@ public class MstKaryawanDaoImpl implements MstKaryawanDao{
 			rs = ps.executeQuery();
 			while (rs.next()) {
 				MstKaryawan mstKaryawan = new MstKaryawan();
-				mstKaryawan.setNik(rs.getString(""));
-				mstKaryawan.setNamaKaryawan(rs.getString(""));
-				mstKaryawan.setJenisKelamin(rs.getString(""));
-				mstKaryawan.setNoAbsen(rs.getString(""));
-				mstKaryawan.setKodePerusahaan(rs.getString(""));
-				mstKaryawan.setTanggalMasuk(rs.getDate(""));
-				mstKaryawan.setNoRekening(rs.getString(""));
-				
+				mstKaryawan.setNik(rs.getString("NIK"));
+				mstKaryawan.setNamaKaryawan(rs.getString("Nama_Karyawan"));
+				mstKaryawan.setJenisKelamin(rs.getString("Jenis_Kelamin"));
+				mstKaryawan.setNoAbsen(rs.getString("No_Absen"));
+				mstKaryawan.setKodePerusahaan(rs.getString("Kode_Perusahaan"));
+				mstKaryawan.setTanggalMasuk(rs.getDate("Tanggal_Masuk"));
+				mstKaryawan.setNoRekening(rs.getString("No_Rekening"));
+
 				listKaryawan.add(mstKaryawan);
+				
+				
+				
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
@@ -81,14 +166,47 @@ public class MstKaryawanDaoImpl implements MstKaryawanDao{
 
 	@Override
 	public MstKaryawan findone(String nik) {
-		// TODO Auto-generated method stub
-		return null;
+		String query = "SELECT * FROM dbo.Table_Karyawan where NIK = '" + nik+"'";
+		Connection con = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		MstKaryawan mstKaryawan = new MstKaryawan();
+		try {
+			con = dataSource.getConnection();
+			ps = con.prepareStatement(query);
+			rs = ps.executeQuery();
+			while (rs.next()) {
+		mstKaryawan.setNik(rs.getString("NIK"));
+		mstKaryawan.setNamaKaryawan(rs.getString("Nama_Karyawan"));
+		mstKaryawan.setJenisKelamin(rs.getString("Jenis_Kelamin"));
+		mstKaryawan.setNoAbsen(rs.getString("No_Absen"));
+		mstKaryawan.setKodePerusahaan(rs.getString("Kode_Perusahaan"));
+		mstKaryawan.setTanggalMasuk(rs.getDate("Tanggal_Masuk"));
+		mstKaryawan.setNoRekening(rs.getString("No_Rekening"));
+		
+					
+				
+				
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				rs.close();
+				ps.close();
+				con.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return mstKaryawan;
 	}
 
 	@Override
 	public List<MstKaryawan> Search(String search) {
-		String query = "select * from Table_Karyawan WHERE NAMA_KARYAWAN LIKE '%"+search+"%'";
-		List<MstKaryawan> listKaryawan= new ArrayList<>();
+		String query = "select * from Table_Karyawan WHERE NAMA_KARYAWAN LIKE '%"
+				+ search + "%'";
+		List<MstKaryawan> listKaryawan = new ArrayList<>();
 		Connection con = null;
 		PreparedStatement ps = null;
 		ResultSet rs = null;
@@ -98,22 +216,22 @@ public class MstKaryawanDaoImpl implements MstKaryawanDao{
 			ps = con.prepareStatement(query);
 			rs = ps.executeQuery();
 			while (rs.next()) {
-				MstKaryawan mstKaryawan= new MstKaryawan();
+				MstKaryawan mstKaryawan = new MstKaryawan();
 
-//				mstKaryawan.setnamaKaryawan(rs.getString(""));
-//				mstKaryawan.setnik(rs.getString(""));
-//				mstKaryawan.setnoRek(rs.getString(""));
-//				
-//					
-//				
-//				kodeProject = (rs.getString("kode_customer"));
-//				mstProject = mstProjectDao.findOne(namaProject);
-//				mstKaryawan.setnamaProject(mstProject);
-//
-//
-//				
-//				trHeader.setKodeKaryawan(mstKaryawan);
-//				listKaryawan;.add(mstKaryawan);
+				// mstKaryawan.setnamaKaryawan(rs.getString(""));
+				// mstKaryawan.setnik(rs.getString(""));
+				// mstKaryawan.setnoRek(rs.getString(""));
+				//
+				//
+				//
+				// kodeProject = (rs.getString("kode_customer"));
+				// mstProject = mstProjectDao.findOne(namaProject);
+				// mstKaryawan.setnamaProject(mstProject);
+				//
+				//
+				//
+				// trHeader.setKodeKaryawan(mstKaryawan);
+				// listKaryawan;.add(mstKaryawan);
 			}
 		} catch (SQLException e) {
 			// TODO: handle exception
